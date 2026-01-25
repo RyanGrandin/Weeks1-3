a7163cb534ae70ec50ed2ea5e59793bfefc9df9d
Ryan Grandin <rmgrandin8@gmail.com>
2026-01-25 16:30:09 -0500

    Required reading 4.3. Within the TankSpawner script. made a new GameObject variable called spawnedTank that equals the tank instance made when the left mouse button is pressed. Added an integer variable called tankCount which increases whenever a new tank is instantiated. Made a new FirstScript object called tankScript that equals the FirstScript of spawnedTank. Made the speed of tankScript equal tankCount. Now the speed of each new tank clone equals the number tank clones that exist at the time it is created inclusively.

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,6 +4,11 @@ using UnityEngine.InputSystem;
 public class TankSpawner : MonoBehaviour
 {
     public GameObject tankPrefab;
+    public int tankCount;
+    public GameObject spawnedTank;
+
+    public FirstScript tankScript;
+    public SpriteRenderer tankSR;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +23,14 @@ public class TankSpawner : MonoBehaviour
         {
             // Instantiate(tankPrefab, transform.position, transform.rotation);
             Vector2 spawnPos = Random.insideUnitCircle * 3;
-            Instantiate(tankPrefab, spawnPos, Quaternion.identity);
+            spawnedTank = Instantiate(tankPrefab, spawnPos, Quaternion.identity);
+
+            tankScript = spawnedTank.GetComponent<FirstScript>();
+            tankSR = spawnedTank.GetComponent<SpriteRenderer>();
+
+            tankCount++;
+
+            tankScript.speed = tankCount;
         }
     }
 }
