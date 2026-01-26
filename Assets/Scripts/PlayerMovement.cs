93ef2ab10c87a42bcb926d16ec584adc9f2a2935
Ryan Grandin <rmgrandin8@gmail.com>
2026-01-26 17:34:08 -0500

    Wrote the player movement script so that the player/object's x position matches the cursor's x position.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +12,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // get mouse position
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        // convert object position to a vector2
+        Vector2 newPos = transform.position;
+        // set object x position to equal mouse x position
+        newPos.x = mousePos.x;
+        // apply position changes back to the object's real position
+        transform.position = newPos;
     }
 }
