727b102a3644ab07582ff04032a074d07af297a4
Ryan Grandin <rmgrandin8@gmail.com>
2026-01-27 14:20:27 -0500

    Used speed to make snowflake lerp on its own. Set snowflake's start and end points to somewhere random at the top and bottom of the screen respectively. However, the end point matches the start point for some reason, so even though time is moving forward, the snowflake is stuck at its random position at the top.

--- Assets/Scripts/SnowflakeMovement.cs
+++ Assets/Scripts/SnowflakeMovement.cs
@@ -2,21 +2,27 @@ using UnityEngine;
 
 public class SnowflakeMovement : MonoBehaviour
 {
-    public Vector2 startPos;
-    public Vector2 endPos;
+    private Vector2 startScreenPos;
+    private Vector2 endScreenPos;
+    public Vector2 startWorldPos;
+    public Vector2 endWorldPos;
     public float t;
+    public float speed = 0.1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // set random snowflake starting and ending x positons
-        //startPos.Set(Random.Range(0, Screen.width), Screen.height);
-        //endPos.Set(Random.Range(0, Screen.width), 0);
+        startScreenPos.Set(Random.Range(0, Screen.width), Screen.height);
+        endScreenPos.Set(Random.Range(0, Screen.width), 0);
+        startWorldPos = Camera.main.ScreenToWorldPoint(startScreenPos);
+        endWorldPos = Camera.main.ScreenToWorldPoint(startScreenPos);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.Lerp(startPos, endPos, t);
+        transform.position = Vector2.Lerp(startWorldPos, endWorldPos, t);
+        t += speed * Time.deltaTime;
     }
 }
