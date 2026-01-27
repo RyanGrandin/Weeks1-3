976117b62c739e1218afef6b568a5b8604fc8a5f
Ryan Grandin <rmgrandin8@gmail.com>
2026-01-27 15:32:11 -0500

    Once the snowflake reaches the bottom of the screen, it is reset to its starting position, its start and end positions are randomised again, and it gets a random speed.

--- Assets/Scripts/SnowflakeMovement.cs
+++ Assets/Scripts/SnowflakeMovement.cs
@@ -17,6 +17,9 @@ public class SnowflakeMovement : MonoBehaviour
         endScreenPos.Set(Random.Range(0, Screen.width), 0);
         startWorldPos = Camera.main.ScreenToWorldPoint(startScreenPos);
         endWorldPos = Camera.main.ScreenToWorldPoint(endScreenPos);
+
+        // give snowflake a random speed
+        speed = Random.Range(0.2f, 0.6f);
     }
 
     // Update is called once per frame
@@ -24,5 +27,17 @@ public class SnowflakeMovement : MonoBehaviour
     {
         transform.position = Vector2.Lerp(startWorldPos, endWorldPos, t);
         t += speed * Time.deltaTime;
+        // once the snowflake has reached the bottom of the screen, set it back to its starting positon and give it a new start 
+        // and end position
+        if (t > 1)
+        {
+            t = 0;
+            startScreenPos.Set(Random.Range(0, Screen.width), Screen.height);
+            endScreenPos.Set(Random.Range(0, Screen.width), 0);
+            startWorldPos = Camera.main.ScreenToWorldPoint(startScreenPos);
+            endWorldPos = Camera.main.ScreenToWorldPoint(endScreenPos);
+            // give snowflake a random speed
+            speed = Random.Range(0.2f, 0.6f);
+        }
     }
 }
