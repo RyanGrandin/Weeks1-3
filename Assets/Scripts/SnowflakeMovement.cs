9b356b010485f154ac3ebb2cddbdd818bcd316a3
Ryan Grandin <rmgrandin8@gmail.com>
2026-01-27 11:19:02 -0500

    Made snowflake lerp from the top of my screen to the bottom. Still need to chnage it so that it will work for every screen.

--- Assets/Scripts/SnowflakeMovement.cs
+++ Assets/Scripts/SnowflakeMovement.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class SnowflakeMovement : MonoBehaviour
 {
+    public Vector2 startPos;
+    public Vector2 endPos;
+    public float t;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // set random snowflake starting and ending x positons
+        //startPos.Set(Random.Range(0, Screen.width), Screen.height);
+        //endPos.Set(Random.Range(0, Screen.width), 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.Lerp(startPos, endPos, t);
     }
 }
