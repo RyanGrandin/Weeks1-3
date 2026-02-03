e0dc874cfc47945cdf596654b29892cba84e3f6d
Ryan Grandin <rmgrandin8@gmail.com>
2026-02-03 18:06:31 -0500

    required reading 5.0. Added a duck to the UI that changes colour along with the square (same colour). Added a functioning click counter to the corner of the UI.

--- Assets/Scripts/UIDemo.cs
+++ Assets/Scripts/UIDemo.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIDemo : MonoBehaviour
 {
     private SpriteRenderer SR;
+    public Image duckImage;
+    public int clickCount = 0;
+    public TextMeshProUGUI score;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SR = GetComponent<SpriteRenderer>();
+        score.text = clickCount.ToString();
     }
 
     // Update is called once per frame
@@ -23,10 +29,17 @@ public class UIDemo : MonoBehaviour
     public void ChangeColour()
     {
         SR.color = Random.ColorHSV();
+        duckImage.color = SR.color;
     }
 
     public void SetScaleBig(float scale)
     {
         transform.localScale = Vector3.one * scale;
+    }
+
+    public void countClicks()
+    {
+        clickCount++;
+        score.text = clickCount.ToString();
     }
 }
