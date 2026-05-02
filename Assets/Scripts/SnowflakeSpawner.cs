a17b6821a8d25a18557178290e15903f3a16979e
Ryan Grandin <rmgrandin8@gmail.com>
2026-05-02 13:33:08 -0400

    Made the player get smaller and move down when the distance between it and the right side of the screen decreases. Still very scuffed though. Mainly, the scale change is too drastic.

--- Assets/Scripts/SnowflakeSpawner.cs
+++ Assets/Scripts/SnowflakeSpawner.cs
@@ -10,6 +10,7 @@ public class SnowflakeSpawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // instantiates 15 snowflake prefab gameobjects and adds them to the snowflake list
         for (int i = 0; i < 15; i++)
         {
             spawnedSnowflake = Instantiate(snowflakePrefab);
