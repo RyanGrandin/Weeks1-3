26fbbb9ea82201ecf7766be57c75fed0770a9425
Ryan Grandin <rmgrandin8@gmail.com>
2026-05-08 17:38:25 -0400

    Changed the shovelling area from a triangle to a square and settled on SpriteRenderer.bounds.Contains(). Added a snow dropping area using the same method as the shovelling area. Added shovelled snow object behind the shovel's scoop. Got all functionality between the two areas, the shovel and the shovelled snow working. This includes the shovel's sprite changng while the scoop is in the droppign area. Added a canvas for the start of the game with text and a button. The only code that runs before the button is pressed is the snowflake movement. Once play has started, the first canvas is deactivated a differrent one is set active. The second canvas has a textmp that shows how much snow the player has shovelled.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,38 +3,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Vector2 screenRightSide = new Vector2(Screen.width, Screen.height);
-    //Vector2 worldRightSide = new Vector2();
-    //public float distance = 0;
+    public bool play = false;
+    public GameObject startCanvasGO;
+    public GameObject playCanvasGO;
 
 // Start is called once before the first execution of Update after the MonoBehaviour is created
 void Start()
     {
-        //worldRightSide = Camera.main.ScreenToWorldPoint(screenRightSide);
+        startCanvasGO.SetActive(true);
+        playCanvasGO.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // get mouse position
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        // convert object position to a vector2
-        Vector2 newPos = transform.position;
-        // set object x position to equal mouse x position
-        newPos.x = mousePos.x;
-        // apply position changes back to the object's real position
-        transform.position = newPos;
+        if (play) 
+        {
+            // get mouse position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            // convert object position to a vector2
+            Vector2 newPos = transform.position;
+            // set object x position to equal mouse x position
+            newPos.x = mousePos.x;
+            // apply position changes back to the object's real position
+            transform.position = newPos;
+        }
+    }
 
-        //worldRightSide.Set(worldRightSide.x, transform.position.y);
-        //distance = Vector2.Distance(worldRightSide, transform.position);
-
-        //Vector2 newScale = transform.localScale;
-        //newScale.x = distance / 10 * 2;
-        //newScale.y = distance / 10 * 2;
-        //transform.localScale = newScale;
-
-        //Vector2 newNewPos = transform.position;
-        //newNewPos.y = distance / 5 - 5;
-        //transform.position = newNewPos;
+    public void StartPlay()
+    {
+        play = true;
+        startCanvasGO.SetActive(false);
+        playCanvasGO.SetActive(true);
     }
 }
--- Assets/Scripts/ShovelMovement.cs
+++ Assets/Scripts/ShovelMovement.cs
@@ -1,46 +1,75 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ShovelMovement : MonoBehaviour
 {
-    //public SpriteRenderer shovelSR;
     public bool carrySnow = false;
-    public SpriteRenderer acornPileAreaSR;
+    public PlayerMovement playerMovement;
     Vector2 shovelScoopPosition = new Vector2();
+    public SpriteRenderer acornPileAreaSR;
+    public SpriteRenderer snowDropAreaSR;
+    public SpriteRenderer shovelSR;
+    public Sprite scoopDefault;
+    public Sprite scoopTipped;
+    public GameObject shovelledSnow;
+    int shovelledCount = 0;
+    public TextMeshProUGUI shovelledCountDisplay;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        acornPileAreaSR.color = new Color(acornPileAreaSR.color.r, acornPileAreaSR.color.g, acornPileAreaSR.color.b, 0);
+        snowDropAreaSR.color = new Color(snowDropAreaSR.color.r, snowDropAreaSR.color.g, snowDropAreaSR.color.b, 0);
+        shovelledSnow.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // get mouse position
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        // convert object position to a vector2
-        Vector2 newPos = transform.position;
-        // set object y position to equal mouse y position
-        newPos.y = Mathf.Clamp(mousePos.y, -3, -1);
-        // apply position changes back to the object's real position
-        transform.position = newPos;
-
-        //if (transform.position.magnitude.Equals(Camera.main.ScreenToWorldPoint(new Vector2(Screen.width / 2, Screen.height / 4))))
-        //{
-        //    shovelSR.color = Color.green;
-        //}
-
-        shovelScoopPosition.Set(transform.position.x-2, transform.position.y+0.5f);
-
-        if (acornPileAreaSR.sprite.bounds.Contains(shovelScoopPosition))
+        if (playerMovement.play)
         {
-            carrySnow = true;
-        }
-        else
-        {
-            carrySnow = false;
+            shovelScoopPosition.Set(transform.position.x - 2, transform.position.y + 0.5f);
+
+            if (acornPileAreaSR.bounds.Contains(shovelScoopPosition))
+            {
+                carrySnow = true;
+            }
+
+            if (snowDropAreaSR.bounds.Contains(shovelScoopPosition))
+            {
+                shovelSR.sprite = scoopTipped;
+                if (carrySnow)
+                {
+                    shovelledCount += 1;
+                    carrySnow = false;
+                }
+            }
+            else
+            {
+                shovelSR.sprite = scoopDefault;
+            }
+
+            if (carrySnow)
+            {
+                shovelledSnow.SetActive(true);
+            }
+            else
+            {
+                shovelledSnow.SetActive(false);
+            }
+
+            shovelledCountDisplay.text = "snow shovelled = " + shovelledCount + "Kg";
+
+            // get mouse position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            // convert object position to a vector2
+            Vector2 newPos = transform.position;
+            // set object y position to equal mouse y position
+            newPos.y = Mathf.Clamp(mousePos.y, -3, -1);
+            // apply position changes back to the object's real position
+            transform.position = newPos;
         }
     }
 }
